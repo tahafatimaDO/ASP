f4177683e78f6d3cd4d3fd608602debadb9aba2e
taha-fatima <tahafatima671@gmail.com>
2021-12-04 07:21:43 +0530

    test

---
+++ Pages/Another.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Pages
+{
+    public class AnotherModel : PageModel
+    {
+        private readonly ILogger<AnotherModel> _logger;
+
+        public AnotherModel(ILogger<AnotherModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
